b0ae18a0bf8c90b731af732de8c1b2193336318f
Adam Poulemanos <07-framer.boons@icloud.com>
2024-07-07 13:59:36 -0400

    such empty

---
+++ custom_extensions/custom_loader.py
@@ -0,0 +1,3 @@
+import markdown
+
+extensions = markdown.Markdown(extensions=["pymdownx.blocks.admonition", "pymdownx.blocks.details", "pymdownx.blocks.definition", "pymdownx.blocks.html", "pymdownx.blocks.tab"])
