f34628fd715fb12bb48f7d74cd440b346fde4362
Adam Poulemanos <adam@plainlicense.org>
2024-09-17 22:22:50 -0400

    debugging submodule update shellscript

--- .github/scripts/update-submodules.sh
+++ .github/scripts/update-submodules.sh
@@ -15,6 +15,10 @@ submodules["mkdocs-material,branch"]="master"
 submodules["mkdocs-material,sparse_paths"]="/material/templates/assets/javascripts /material/templates/assets/stylesheets"
 
 CWD="$(pwd)"
+GIT_DIR="$(git rev-parse --git-dir)"
+GIT_DIR_ABS_PATH="$(git rev-parse --absolute-git-dir)"
+REPO_ROOT_ABS_PATH="$(git rev-parse --show-toplevel)"
+SUBMODULE_PATH_PREFIX='external'
 
 # Display error messages
 error_exit() {
@@ -22,21 +26,20 @@ error_exit() {
     exit 1
 }
 
-double_check_directory() {
-    # Since we are about to hard reset, we MAKE ABSOLUTELY SURE
-    # That we are NOT in the root of the repository
-    local expected_dir="$1"
-
-    local top_level_dir="$(git rev-parse --show-toplevel)"
+# Function to check if we are in the root directory
+is_this_root() {
     local cwd="$(pwd)"
-
-    if [[ "${top_level_dir}" == "${cwd}" ]]; then
-        error_exit "You are in the root of the repository. Please change to a subdirectory. Exiting..."
+    if [[ -z "${REPO_ROOT_ABS_PATH}" ]]; then
+        return 1
     fi
-    if [[ cwd != expected_dir ]]; then
-        error_exit "You are not in the expected directory. Please change to the correct directory. Exiting..."
-    fi
+    return 0
+    }
 
+reroute_to_dir() {
+    local expected_dir="$1"
+    if [[ "${cwd}" != "${expected_dir}" ]]; then
+        cd "${expected_dir}" || error_exit "Failed to change directory to ${expected_dir}. Exiting..."
+    fi
     echo "Directory check passed."
     return 0
 }
@@ -44,15 +47,20 @@ double_check_directory() {
 # Enable sparse checkout
 enable_sparse_checkout() {
     local sparse_paths="$1"
-
+    reroute_to_dir "${SUBMODULE_ABS_PATH}" || error_exit "Failed to change directory to submodule."
     echo "Enabling sparse checkout..."
     git config core.sparseCheckout true
+    SPARSE_CHECKOUT_FILE="${GIT_DIR_ABS_PATH}/modules/${SUBMODULE_REL_PATH}/info/sparse-checkout"
+    echo "Attempting to write sparse checkout for ${SPARSE_CHECKOUT_FILE}..."
 
-    GIT_DIR="$(git rev-parse --git-dir)"
-    SPARSE_CHECKOUT_FILE="${GIT_DIR}/info/sparse-checkout"
+    if [[ ! -f "${SPARSE_CHECKOUT_FILE}" ]]; then
+        echo "Creating sparse-checkout file..."
+        touch "${SPARSE_CHECKOUT_FILE}" || error_exit "Failed to create sparse-checkout file."
+    fi
+    update_sparse_checkout "${sparse_paths}" "${SPARSE_CHECKOUT_FILE}" || error_exit "Failed to update sparse-checkout."
 
-    # Overwrite the sparse-checkout file
-    printf "%s\n" "${sparse_paths}" > "${SPARSE_CHECKOUT_FILE}"
+    echo "Sparse checkout enabled."
+    return 0
 }
 
 # Create/add submodule if it doesn't exist
@@ -61,10 +69,12 @@ create_or_add_submodule() {
     local remote_url="$2"
     local branch="$3"
 
-    if [[ ! -d "${submodule}" ]]; then
+    if [[ ! -d "${SUBMODULE_REL_PATH}" ]]; then
         echo "Submodule not found. Adding submodule..."
-        git submodule add -b "${branch}" "${remote_url}" "${submodule}" || error_exit "Failed to add submodule."
-        git submodule update --init --recursive "${submodule}" || error_exit "Failed to initialize submodule."
+        reroute_to_dir "${REPO_ROOT_ABS_PATH}" || error_exit "Failed to change directory to repo root."
+        git submodule add -b "${branch}" "${remote_url}" "${SUBMODULE_REL_PATH}" || error_exit "Failed to add submodule."
+        reroute_to_dir "${SUBMODULE_ABS_PATH}" || error_exit "Failed to change directory to submodule."
+        git submodule update --init --recursive || error_exit "Failed to update submodule."
     fi
 }
 
@@ -74,21 +84,19 @@ check_sparse_checkout_file() {
     local sparse_checkout_file="$2"
 
     if [[ ! -f "${sparse_checkout_file}" ]]; then
-        echo "Sparse-checkout file not found."
-        return 2  # Error code for missing file
+        enable_sparse_checkout "${sparse_paths}"
+        return 0
     fi
 
-    local current_paths
     mapfile -t current_paths < "${sparse_checkout_file}"
-
-    local expected_paths=("${sparse_paths}")
+    IFS=' ' read -r -a expected_paths <<< "${sparse_paths}"
 
     if [[ "${#current_paths[@]}" -ne "${#expected_paths[@]}" ]]; then
         return 1  # Paths do not match
     fi
 
     for i in "${!current_paths[@]}"; do
-        if [[ "${current_paths[$i]}" != "${expected_paths[$i]}" ]]; then
+        if [[ "${current_paths[${i}]}" != "${expected_paths[${i}]}" ]]; then
             return 1  # Paths do not match
         fi
     done
@@ -100,7 +108,7 @@ check_sparse_checkout_file() {
 update_sparse_checkout() {
     local sparse_paths="$1"
     local sparse_checkout_file="$2"
-
+    reroute_to_dir "${SUBMODULE_ABS_PATH}" || error_exit "Failed to change directory to submodule."
     echo "Updating sparse checkout..."
     printf "%s\n" "${sparse_paths}" > "${sparse_checkout_file}"
     git read-tree -mu HEAD
@@ -132,7 +140,7 @@ check_files_match_sparse_checkout() {
     mapfile -t sorted_actual < <(printf "%s\n" "${actual_files[@]}" | sort)
 
     for i in "${!sorted_expected[@]}"; do
-        if [[ "${sorted_expected[$i]}" != "${sorted_actual["$i"]}" ]]; then
+        if [[ "${sorted_expected[${i}]}" != "${sorted_actual[${i}]}" ]]; then
             return 1  # Files do not match
         fi
     done
@@ -144,6 +152,7 @@ check_files_match_sparse_checkout() {
 sync_files_with_sparse_checkout() {
     local sparse_paths="$1"
     local branch="$2"
+    local submodule="$3"
 
     GIT_DIR="$(git rev-parse --git-dir)"
     SPARSE_CHECKOUT_FILE="${GIT_DIR}/info/sparse-checkout"
@@ -161,13 +170,11 @@ sync_files_with_sparse_checkout() {
     check_files_match_sparse_checkout "${sparse_paths}"
     status=$?
     if [[ ${status} -eq 1 ]]; then
-        local expected_dir="${sparse_paths%%/*}"
+        echo "Stashing changes..."
+        git stash save "Going to hard reset submodule..." || error_exit "Failed to stash changes."
         echo "Files do not match sparse-checkout. Resetting submodule..."
-        if [[ ! $(set -e && double_check_directory ${expected_dir}) -ne 0 ]]; then
-            error_exit "Error occurred in double_check_directory."
-        fi
-        echo "stashing changes..."
-        git stash save "going to hard reset submodule..." || error_exit "Failed to stash changes."
+        reroute_to_dir "${SUBMODULE_ABS_PATH}" || error_exit "Failed to change directory to submodule."
+
         git reset --hard "origin/${branch}" || error_exit "Failed to reset submodule."
         git clean -fdx || error_exit "Failed to clean submodule."
         git read-tree -mu HEAD || error_exit "Failed to update working tree."
@@ -182,18 +189,21 @@ update_submodule() {
     local remote_url="${submodules[${submodule},url]}"
     local branch="${submodules[${submodule},branch]}"
     local sparse_paths="${submodules[${submodule},sparse_paths]}"
+    local cwd="$(pwd)"
 
     echo "Updating submodule at ${submodule}..."
 
+    reroute_to_dir "${REPO_ROOT_ABS_PATH}" || error_exit "Failed to change directory to repo root."
+
     create_or_add_submodule "${submodule}" "${remote_url}" "${branch}"
-    cd "${submodule}" || error_exit "Failed to enter submodule directory."
+    cd "${SUBMODULE_ABS_PATH}" || error_exit "Failed to enter submodule directory."
 
     # Restore write permissions
     chmod -R u+w . || error_exit "Failed to set write permissions."
 
     if [[ -n "${sparse_paths}" ]]; then
         enable_sparse_checkout "${sparse_paths}"
-        sync_files_with_sparse_checkout "${sparse_paths}" "${branch}"
+        sync_files_with_sparse_checkout "${sparse_paths}" "${branch}" "${submodule}"
     fi
 
     # Fetch latest changes
@@ -202,17 +212,16 @@ update_submodule() {
         error_exit "Failed to fetch updates from 'origin/${branch}'."
     fi
 
-    # Update to the latest commit
-    git reset --hard "origin/${branch}" || error_exit "Failed to reset to latest commit."
-
     # Update working tree
+    echo "Updating working tree..."
+    reroute_to_dir "${SUBMODULE_ABS_PATH}" || error_exit "Failed to change directory to submodule."
     git read-tree -mu HEAD || error_exit "Failed to update working tree."
 
     # Set submodule to read-only after update
     chmod -R a-w . || error_exit "Failed to set read-only permissions."
 
     echo "Submodule ${submodule} updated successfully."
-    cd "${CWD}" || error_exit "Failed to return to the main repository."
+    cd "${REPO_ROOT_ABS_PATH}" || error_exit "Failed to return to the main repository."
 }
 
 # Collect unique submodule names
@@ -224,5 +233,13 @@ done
 
 # Iterate over unique submodule names
 for submodule in "${!submodule_names[@]}"; do
+    echo "Processing submodule: ${submodule}"
+    if [[ "${SUBMODULE_PATH_PREFIX}" != '/' ]]; then
+        SUBMODULE_REL_PATH="${SUBMODULE_PATH_PREFIX}/${submodule}"
+        SUBMODULE_ABS_PATH="${REPO_ROOT_ABS_PATH}/${SUBMODULE_REL_PATH}"
+    else
+        SUBMODULE_REL_PATH="${submodule}"
+        SUBMODULE_ABS_PATH="${REPO_ROOT_ABS_PATH}/${submodule}"
+    fi
     update_submodule "${submodule}"
 done
